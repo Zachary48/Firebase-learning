9271dc5515652138ed02ce75b9458053d066efb4
Zachary48 <158526110+Zachary48@users.noreply.github.com>
2024-11-19 21:51:32 -0600

    Initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlfTTigRUJeEP34LH-3PqlKP0uvyDhVY4",
+    authDomain: "learning-firebase-26b79.firebaseapp.com",
+    projectId: "learning-firebase-26b79",
+    storageBucket: "learning-firebase-26b79.firebasestorage.app",
+    messagingSenderId: "25681928773",
+    appId: "1:25681928773:web:412c1d3a3b3462a7def92c",
+    measurementId: "G-587BD71QK1"
+  };
